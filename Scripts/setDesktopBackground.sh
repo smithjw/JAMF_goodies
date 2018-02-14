ee25d6c9d2d45ac52af7defd718945ca1f597656
James Smith <james@smithjw.me>
2018-02-15 09:29:18 +1100

    Added some comments for future me

--- Scripts/setDesktopBackground.sh
+++ Scripts/setDesktopBackground.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Create an empty array
+# Create an array with the contents of the CA Wallpapers filder
 wallpapers=(/Library/Desktop\ Pictures/CA\ Wallpapers/*)
+# Seed a random number
 RANDOM=$$$(date +%s)
 
+# Select one of the wallpapers from the array
 selectedWallpaper=${wallpapers[$RANDOM % ${#wallpapers[@]} ]}
 
 echo "${selectedWallpaper}"
 
+# Set this wallpaper during enrolment
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"$selectedWallpaper"'"'
 
 exit 0
