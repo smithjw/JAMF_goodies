0952494149a0860f5a4803a4cafbf6f075c4b115
James Smith <james@smithjw.me>
2018-02-15 09:26:10 +1100

    Added some randomisation into the selection of CAmper Wallpapers

--- Scripts/setDesktopBackground.sh
+++ Scripts/setDesktopBackground.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/CA-wallpapers-all-hands.png"'
+# Create an empty array
+wallpapers=(/Library/Desktop\ Pictures/CA\ Wallpapers/*)
+RANDOM=$$$(date +%s)
+
+selectedWallpaper=${wallpapers[$RANDOM % ${#wallpapers[@]} ]}
+
+echo "${selectedWallpaper}"
+
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"$selectedWallpaper"'"'
 
 exit 0
